79f74677d87f56b355b85588cbe1a62da16f9468
Kiet <ducvub10@gmail.com>
2023-06-30 15:58:20 +0700

    Lecture 34

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import ProfileCard from "./ProfileCard";
+import alexaImage from './images/alexa.png';
+import cortanaImage from './images/cortana.png';
+import siriImage from './images/siri.png';
 
+console.log(alexaImage);
+console.log(siriImage);
 function App(){
     return(
         <div>
             <div>Personal Digital Assistants</div>
-            
+            <img src = {alexaImage} />
+            <img src = "https://picsum.photos/200/300" />
             <ProfileCard title = 'Alexa' handle = '@alexa99'/>
             <ProfileCard title = 'Cortana' handle = '@cortana32'/>
             <ProfileCard title = 'Siri' handle = '@siri01'/>

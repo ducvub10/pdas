0ee4fcc72b0a0e4660be77022743332e052e306c
Kiet <ducvub10@gmail.com>
2023-06-30 15:29:14 +0700

    Lecture 29

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,4 +1,13 @@
-function ProfileCard(){
-    return <div>Profile Card!!!</div>
+function ProfileCard({title, handle}){
+    // const title = props.title;
+    // const handle = props.handle;
+    
+    return (
+        
+        <div>
+            <div>Title is {title}</div>
+            <div>Handle is {handle}</div>
+        </div>
+    )
 }
 export default ProfileCard
